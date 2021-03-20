3d74d0ed4b0071de719a4efc16ee0fb5674b52b4
GhadaRagab123 <ghadar109@gmail.com>
2021-03-20 02:19:35 +0200

    orderpage

--- YallaNotlob/config/routes.rb
+++ YallaNotlob/config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
-  devise_for :users
+  # devise_for :users
+  root "orders#index"
+  # resources :orders
+  post "/orders", to: "orders#create"
+  get "/orders", to: "orders#create" 
+ 
+  
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
