13fbfeb52c8ab6048995a13aaaf602c3eab0908a
mohamedkaoud14 <mdkaoud14@gmail.com>
2021-03-23 11:21:11 -0400

    uploading image

--- YallaNotlob/app/controllers/application_controller.rb
+++ YallaNotlob/app/controllers/application_controller.rb
@@ -1,8 +1,15 @@
 class ApplicationController < ActionController::Base  
     before_action :authenticate_user!
     before_action :configure_permitted_parameters, if: :devise_controller?
+        protected 
 
     def configure_permitted_parameters
         devise_parameter_sanitizer.permit(:sign_up, keys: [:username])
+        devise_parameter_sanitizer.permit(:sign_up,keys: [:avatar])
+            devise_parameter_sanitizer.permit(:account_update,keys: [:avatar])
       end
+
+      # before_action :configure_premitted_parameters, 
+      #  if: :devise_controller?
+
 end
--- YallaNotlob/app/models/user.rb
+++ YallaNotlob/app/models/user.rb
@@ -3,7 +3,34 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
 devise :database_authenticatable, :registerable, :recoverable, :rememberable, :validatable, :omniauthable, omniauth_providers: [:facebook, :github, :google_oauth2]
 
+  has_one_attached :avatar
 
+after_create_commit :add_default_avatar
+
+  def  avatar_thumbnail
+      if avatar.attached?
+    avatar.variant(resize: "150x150!").processed 
+      else
+        "/default.jpeg"
+
+      end
+  end 
+
+  private
+  def add_default_avatar
+    unless avatar.attached?
+      avatar.attach(
+        io: File.open(
+          Rails.root.join(
+
+          'app', 'assets', 'images' , 'default.jpeg'
+          )
+
+        ), filename: 'default.jpeg' ,
+        content_type: 'image/jpeg'
+      )
+    end
+  end
    
 
    def self.create_from_provider_data(provider_data) 
